48b3e27710f9fd61e4e0f900f672de5c75125e58
Asini-Jayakody <jayakodyasini@gmail.com>
2025-03-20 23:50:11 +0530

    error in backend url

--- UI/cyber_ui/src/Config.js
+++ UI/cyber_ui/src/Config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: "https://2tpdfwkyeuvmndlb6thth5qmfa0wzjuz.lambda-url.us-east-1.on.aws/"
+    API_BASE_URL: "https://2tpdfwkyeuvmndlb6thth5qmfa0wzjuz.lambda-url.us-east-1.on.aws"
     // API_BASE_URL: "http://127.0.0.1:8000"
   };
   
